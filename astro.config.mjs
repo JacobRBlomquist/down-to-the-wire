3bbd1c33b08d99b4e83d5ec331922eeb24acdd72
Jacob Blomquist <jacobrb95@gmail.com>
2025-07-18 22:17:08 -0600

    feat(analytics): add google analytics for site traffic monitoring

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import partytown from '@astrojs/partytown'
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	site: 'https://blog.blomquist.xyz',
+	integrations: [
+		mdx(),
+		sitemap(),
+		partytown(
+			{
+				config: {
+					forward: ["dataLayer.push"],
+				}
+			}
+		)
+	],
 });
